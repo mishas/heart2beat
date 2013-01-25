740b5188ff33007621cd58ae17664255da84fb8a
Noam Gat <noamgat@gmail.com>
2013-01-25 18:28:52 +0200

    Win condition

---
+++ UnityProj/Assets/Scripts/WinConditionCollider.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class WinConditionCollider : MonoBehaviour {
+	
+	public Collider targetCollider;
+	public bool hasWon;
+	public Rect textRect = new Rect(50,50,300,50);
+	public GUIStyle textStyle;
+	
+	// Use this for initialization
+	void Start () {
+		hasWon = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other == targetCollider) {
+			hasWon = true;	
+		}
+	}
+	
+	void OnGUI() {
+		if (hasWon) {
+			GUI.Label(textRect, "You Win!", textStyle);
+		}
+	}
+}
