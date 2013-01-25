b2b09ccb39849ae0a0b93f5008b3541e99c89e39
Noam Gat <noamgat@gmail.com>
2013-01-25 17:31:20 +0200

    Ball physics

---
+++ UnityProj/Assets/Scripts/HeartbeatForceGenerator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeartbeatForceGenerator : MonoBehaviour {
+	
+	public HeartBeat heartbeat;
+	public Rigidbody targetBody;
+	public float forceScale = 1f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		HeartBeat.BeatSize beatSize = heartbeat.getBeatAtTime(Time.time);
+		float pulse = beatSize.MechanicalPulse;
+		
+		targetBody.AddForce(Vector3.up * pulse * forceScale);
+	}
+}
