78e509cfde39237381794589b0dcbdd3ce1aaa4f
Noam Gat <noamgat@gmail.com>
2013-01-26 13:45:49 +0200

    Added camera shake

---
+++ UnityProj/Assets/Scripts/HeartbeatShaker.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeartbeatShaker : MonoBehaviour {
+	
+	public HeartbeatHistory heartbeatSource;
+	public float shakeScale = 1;
+	
+	private float nodeVelocity = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HeartBeat.BeatSize beatSize = heartbeatSource.PastBeat;
+		Vector3 localPosition = transform.localPosition;
+		float targetY = beatSize.ElectricalPulse * shakeScale;
+		localPosition.y = Mathf.SmoothDamp(localPosition.y, targetY, ref nodeVelocity, Time.deltaTime);
+		transform.localPosition = localPosition;
+	}
+}
